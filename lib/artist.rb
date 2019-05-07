cfa5b189092a4a1197c4eb9431df0ee449a3c23e
Irina Kagan <irinagamb@gmail.com>
2019-05-07 01:02:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,15 @@
 class Artist
   attr_accessor :name
+  @@all = []
+
+  def self.all
+    @@all
+  end
 
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
   end
 
   def add_song(song)
