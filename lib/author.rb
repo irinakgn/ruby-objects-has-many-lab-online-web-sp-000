a68199c77c819767af7bbf851fcaa2661e1793a2
Irina Kagan <irinagamb@gmail.com>
2019-05-07 01:11:00 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,37 @@
 class Author
   attr_accessor :name
+  @@all = []
+
+  def self.all
+    @@all
+  end
 
   def initialize(name)
     @name = name
-    @posts = []
+    @songs = []
+    @@all << self
   end
 
-  def add_post(post)
-    @posts << post
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def add_song_by_name(name)
+    self.add_song(Song.new(name))
+  end
+
+  def self.song_count
+    count = 0
+    @@all.each{ |artist| count += artist.songs.count }
+    return count
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song_with_name(name)
+    @songs << Song.new(name)
   end
 end
