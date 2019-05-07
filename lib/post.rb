30dc655e6d802bfea636b5ccb090702effce3cdd
Irina Kagan <irinagamb@gmail.com>
2019-05-07 01:13:53 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,19 @@
 class Post
-  attr_accessor :title
+  attr_accessor :artist, :name, :genre
 
   def initialize(title)
     @title = title
+  end
+
+  def authorname_name
+    self.artist.nil? ? nil : self.artist.name
+  end
+
+  def name
+    @name
+  end
+
+  def artist
+    @artist
   end
 end
