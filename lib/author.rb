2196c45a28259ab9d7bdca3ba7ca37cfb2608689
Irina Kagan <irinagamb@gmail.com>
2019-05-07 01:12:19 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,14 +14,14 @@ class Author
 
   def add_song(song)
     song.author = self
-    @songs << song
+    @posts << song
   end
 
-  def add_song_by_name(name)
+  def add_post_by_name(name)
     self.add_post(Post.new(name))
   end
 
-  def self.song_count
+  def self.post_count
     count = 0
     @@all.each{ |author| count += author.posts.count }
     return count
